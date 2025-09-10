929b09221084adcd67dafaab718d93916ce78d85
mridhops <mridhophs@gmail.com>
2025-09-11 04:10:08 +0700

    dev 1.1.0

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            This is Next.js 1.1.0-dev
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/page.js
             </code>
